60919a4820f838e004775add68a7b7a4cc1bbb60
Yuna Won <wonyoona31@gmail.com>
2026-02-23 12:13:51 +0900

    more

---
+++ script.js
@@ -0,0 +1,33 @@
+/*let swiper;
+
+function initSwiper() {
+    if (window.innerWidth < 768 && !swiper) {
+        swiper = new swiper('.brands', {
+            slidesPerView:1,
+            spaceBetween:16,
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: true,
+            },
+        });
+    }
+    if (window.innerWidth >= 768 && swiper) {
+        swiper.destroy(true, true);
+        swiper = underfined;
+    }
+}
+
+document.addEventListener ('DOMContentLoaded', function () {
+    initSwiper();
+    window.addEventListener ('resize', initSwiper);
+})
+
+document.addEventListener('DOMContentLoaded', function(){
+    new Swiper( 'brands', {
+        slidesPerView: 1,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable:true,
+        }
+    })
+})*/
