e1cb55c2c09b7a9f97576e61a37fd0268562567f
Yuna Won <wonyoona31@gmail.com>
2026-02-24 10:08:39 +0900

    save

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
-const toggleBtn = document.getElementById('toggleBtn');
+document.addEventListener('DOMContentLoaded', function () {
+    const toggleBtn = document.getElementById('toggleBtn');
 const text = toggleBtn.querySelector('.text');
-toggleBtn.addEventListener('click', function () {
+function checkWidth() {
+    if (window.innerWidth >= 1120) {
+        toggleBtn.classList.add('active');
+        text.textContent = 'Скрыть';
+    } else {
+        toggleBtn.classList.remove('active');
+        text.textContent = 'Показать все'
+    }
+}
+checkWidth();
+
+window.addEventListener('resize', checkWidth);
+});
+
+
+/*toggleBtn.addEventListener('click', function () {
     toggleBtn.classList.toggle('active');
 
     if(toggleBtn.classList.contains('active')) {
-        text.textContent = 'Скрыть' ;
+        text.textContent = '' ;
     } else {
-        text.textContent = 'Показать все' ;
+        text.textContent = '' ;
     }
-});
+});*/
